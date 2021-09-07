c2595a516d90a8f79647bb759503dbe79be5f8af
Amit Dutta <amitdutta2406@gmail.com>
2021-09-08 01:45:06 +0530

    Updated the codebase using OOP Principles

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -1,8 +1,65 @@
+/*
+    Author : Amit Dutta
+    Program : Conway's Game Of Life
+    Description : This Program prints the Next Tick in the System given the Initial State of System
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// Parse the Input from String and convert it to Pair of integers x, y
-pair<int, int> readPair(string coordinate) {
+class GameOfLife {
+    private:
+        map<pair<int, int>, int> inputMap;
+        map<pair<int, int>, int> resultantMap;
+
+        pair<int, int> readPair(string coordinate);
+        void checkDirections();
+    
+    public:
+        GameOfLife();
+        void getInputs();
+        void printNextTick();
+        void printMapContent(map<pair<int, int>, int> hash);
+        map<pair<int, int>, int> getInputMap();
+        map<pair<int, int>, int> getResultantMap();
+};
+
+
+GameOfLife::GameOfLife() {
+    inputMap.clear();
+    resultantMap.clear();
+}
+
+
+map<pair<int, int>, int> GameOfLife::getInputMap() {
+    return inputMap;
+}
+
+
+map<pair<int, int>, int> GameOfLife::getResultantMap() {
+    return resultantMap;
+}
+
+
+/*
+    Function    : printMapContent()
+    Parameter   : Map<pair<int, int>, int> which is map with key pair<int, int> and value as int
+    Return      : Void
+    Description : Prints either the InputMap which is the Initial State of Sytem or ResultantMap which is the Next Tick of the System
+*/
+void GameOfLife::printMapContent(map<pair<int, int>, int> hash) {
+    for(auto pair : hash) {
+        cout << pair.first.first << ", " << pair.first.second << " - " << pair.second << endl;
+    }
+}
+
+/*
+    Function    : readPair()
+    Parameter   : String - A string of Input in the format x, y where x and y are co-ordinates
+    Return      : A pair of integers x and y
+    Description : It parses the String format in the format x, y using the comma delimeter and returns a pair x and y
+*/
+pair<int, int> GameOfLife::readPair(string coordinate) {
     vector<int> coordinate_vector;
     string str;
     stringstream ss(coordinate);
@@ -26,8 +83,14 @@ pair<int, int> readPair(string coordinate) {
     return {coordinate_vector[0], coordinate_vector[1]};
 }
 
-// Get the desired inputs. Press Enter twice after entering the last coordinate to break the while loop
-void getInputs(map<pair<int, int>, int>& inputMap) {
+
+/*
+    Function    : getInputs()
+    Parameter   : None
+    Return      : Void
+    Description : It takes the Input in String format x, y. After taking the last Input, press Enter twice to exit from the loop
+*/
+void GameOfLife::getInputs() {
     string coordinate;
     while (getline(cin, coordinate)) {
         if (coordinate == "") {
@@ -38,8 +101,14 @@ void getInputs(map<pair<int, int>, int>& inputMap) {
     }
 }
 
-// Check all the 8 adjacent directions for a coordinate, i.e, horizontally, vertically and diagonally
-void checkDirections(map<pair<int, int>, int>& inputMap, map<pair<int, int>, int>& resultantMap) {
+
+/*
+    Function    : checkDirections()
+    Parameter   : None
+    Return      : Voide
+    Description : Check all 8 Adjacent directions for a given cell, i.e, Horizontally, Vertically and Diagonally 
+*/
+void GameOfLife::checkDirections() {
     vector<int> xdir = {0, 0, 1, 1, 1, -1, -1, -1};
     vector<int> ydir = {1, -1, 1, 0, -1, 1, 0, -1};
 
@@ -52,11 +121,18 @@ void checkDirections(map<pair<int, int>, int>& inputMap, map<pair<int, int>, int
             resultantMap[{nx, ny}]++;
         }
     }
-    
 }
 
-// Print the next Tick from the initial set of Live cells
-void printAnswer(map<pair<int, int>, int>& inputMap, map<pair<int, int>, int>& resultantMap) {
+
+/*
+    Function    : printNextTick()
+    Parameter   : None
+    Return      : Void
+    Description : Prints the next State of system following Conway's Game Of lIfe Rules as mentioned in the Github Repo
+*/
+void GameOfLife::printNextTick() {
+    checkDirections();
+
     vector<pair<int, int>> answer;
     for(auto pair : resultantMap) {
         if(pair.second == 3)
@@ -72,13 +148,8 @@ void printAnswer(map<pair<int, int>, int>& inputMap, map<pair<int, int>, int>& r
 }
 
 int main(){
-    map<pair<int, int>, int> inputMap, resultantMap;
-    
-    getInputs(inputMap);
-
-    checkDirections(inputMap, resultantMap);
-
-    printAnswer(inputMap, resultantMap);
-    
+    GameOfLife obj;
+    obj.getInputs();
+    obj.printNextTick();
     return 0;
 }
